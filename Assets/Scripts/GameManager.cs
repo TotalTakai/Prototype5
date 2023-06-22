462e47df9bca338d0ba7eb6264a09eafcc4f4f2b
Nir <yanaynir1@gmail.com>
2023-06-22 15:58:20 +0300

    Adding UI Score Text, and Particle Explosions
    1. Adding UI score text, updating with each object clicked
    2. Adding a particle explosion for each object clicked

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private readonly float spawnRate = 1;
+    private int score;
 
     public List<GameObject> target;
+    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -20,6 +24,7 @@ public class GameManager : MonoBehaviour
         
     }
 
+    // Spawns a random object every second
     private IEnumerator SpawnTarget()
     {
         while (true)
@@ -28,5 +33,12 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, target.Count);
             Instantiate(target[index]);
         }
+    }
+
+    // Adds to the score and prints it on the canvas
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -10,11 +10,16 @@ public class Target : MonoBehaviour
     private readonly float maxSpeed = 15;
     private readonly float maxTorque = 10;
     private Rigidbody targetRb;
+    private GameManager gameManager;
+
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         SetSpawnInfo();
     }
 
@@ -59,6 +64,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     // Destroys the target whenever it reaches the Trigger game object (Sensor)
